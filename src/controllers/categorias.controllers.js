d8f25785b2c48bc5088e300ab82373b9438e7db4
Didier Villamizar <dividiye@gmailcom>
2025-04-06 11:20:10 -0500

    Connecting DB - HTTO : GET

--- src/controllers/categorias.controllers.js
+++ src/controllers/categorias.controllers.js
@@ -1,5 +1,13 @@
-const getCategorias =(req, res) =>{
-  res.json({"Categoria":"Electrodomesticos"})
+import getConnection from "./../db/database.js"
+const getCategorias = async (req, res) =>{
+ try {
+  const connection =  await getConnection();
+  const result = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias")
+  res.json(result);
+ } catch (error) {
+   console.error("Error 500");
+ }
+   
 }
 export const methodHTTP = {
     getCategorias
